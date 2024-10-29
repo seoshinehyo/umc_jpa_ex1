8425a9272523fbd34e922aa2033b767203cee9cb
sshnote <springssh0504@naver.com>
2024-10-29 17:57:58 +0900

    @EnableJpaAuditing 코드 추가

--- src/main/java/umc/jpa_ex1/JpaEx1Application.java
+++ src/main/java/umc/jpa_ex1/JpaEx1Application.java
@@ -2,8 +2,10 @@ package umc.jpa_ex1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JpaEx1Application {
 
 	public static void main(String[] args) {
